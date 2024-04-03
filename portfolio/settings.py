7b70af40c2227a6e51c0ef79aa1119eca97c883f
Guheshp <guheshpanjagall481@gmail.com>
2024-04-03 13:19:51 +0530

    code commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'portfolio',
-        'USER': 'root',
-        'PASSWORD': 'admin123',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
