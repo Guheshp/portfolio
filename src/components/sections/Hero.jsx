a99b51706c019c1d6acc3cfc8afbdb3b649553b3
Guheshp <guheshpanjagall481@gmail.com>
2024-10-02 09:21:14 +0530

    resume 3

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import user_info from "../../data/user_info.js";
 import { IoIosArrowForward } from "react-icons/io";
-import "./../../../public/CV_Guhesh_Panjagall_2024.pdf"
-// import "./../../static/CV_Guhesh_Panjagall_2024.pdf"
+import resumePDF from "../../static/CV_Guhesh_Panjagall_2024.pdf"
+
 function Hero() {
   return (
     <section id="hero" className="pb-28 pt-12 sm:pt-14 md:pt-22 flex px-6 lg:px-24">
@@ -39,8 +39,8 @@ function Hero() {
                 Projects
               </a> */}
               <a
-                href="./../../static/CV_Guhesh_Panjagall_2024.pdf"
-                download="Guhesh_Panjagll_Resume.pdf"
+                href={resumePDF}
+                download="CV_Guhesh_Panjagall_2024.pdf"
                 target="_blank"
                 className="px-6 py-3 border border-custom-navyblue hover:bg-custom-blue hover:text-custom-navyblue hover:border-custom-navyblue dark:border-white rounded-lg font-medium transition-all duration-300"
               >
